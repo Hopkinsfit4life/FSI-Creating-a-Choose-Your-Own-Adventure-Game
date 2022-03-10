c7afae7129a79e0ce8de56df93b68153900cb3d0
Hopkinsfit4life <hopkinsstacey18@gmail.com>
2022-03-10 21:18:47 +0300

    added more to the game

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Stacey Game
+//Stacey's Game
 window.prompt('Do you head left or right?')
 let answer = window.prompt ('Do you head left or right?')
 let firstAnswer  = window.prompt('Do you head left or right?')
@@ -6,11 +6,15 @@ if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
-
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
-
 }
-if thirdAnswer
+if(secondAnswer === 'follow'){
+    let secondAnswer = window.prompt(`You follow the cat to a colony of cats, nestled in a
+    fort of warm blankets and subsisting off of inexplicably warm soup.  They are content with you staying,
+    but you wonder if you should alert the world to this magical safehaven`)
+} else if(secondAnswer === 'Stay') {
+    let thirdAnswer = window.prompt(`You live happily amongst the cats for the rest of your days`)
+}
